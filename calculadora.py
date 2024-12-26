7266fe9388ef119ae106d04601cb4116aef1844e
iaealangodoy <166546286+iaealangodoy@users.noreply.github.com>
2024-12-26 16:38:40 -0300

    Atualizações

--- calculadora.py
+++ calculadora.py
@@ -1,26 +1,39 @@
-numero_1 = input('Digite o primeiro número: ')
-multiplicador = input('Digite um multiplicador: ')
-numero_2 = input('Digite o segundo número: ')
+continuartrue = True
 
-float_numero_1 = float(numero_1)
-float_numero_2 = float(numero_2)
+while continuartrue:
 
-calculo_adicao = float_numero_1 + float_numero_2
-calculo_subtracao = float_numero_1 - float_numero_2
-calculo_multiplicacao = float_numero_1 * float_numero_2
-calculo_divisao = float_numero_1 / float_numero_2
+    numero_1 = input('Digite o primeiro número: ')
+    multiplicador = input('Digite um multiplicador: ')
+    numero_2 = input('Digite o segundo número: ')
 
-if multiplicador == '+':
-    print(f'O resultado da adição é {calculo_adicao:.2f}')
+    float_numero_1 = float(numero_1)
+    float_numero_2 = float(numero_2)
 
-if multiplicador == '-':
-    print(f'O resultado da subtração é {calculo_subtracao:.2f}')
+    calculo_adicao = float_numero_1 + float_numero_2
+    calculo_subtracao = float_numero_1 - float_numero_2
+    calculo_multiplicacao = float_numero_1 * float_numero_2
+    calculo_divisao = float_numero_1 / float_numero_2
 
-if multiplicador == 'x':
-    print(f'O resultado da multiplicação é {calculo_multiplicacao:.2f}')
+    if multiplicador == '+':
+        print(f'O resultado da adição é {calculo_adicao}')
 
-if multiplicador == '/':
-    print(f'O resuldado da divisão é {calculo_divisao:.2f}')
+    elif multiplicador == '-':
+        print(f'O resultado da subtração é {calculo_subtracao}')
 
-else:
-    print('Você precisa digitar algum multiplicador "+", "-", "x" ou "/" para funcionar')
+    elif multiplicador == 'x':
+        print(f'O resultado da multiplicação é {calculo_multiplicacao}')
+
+    elif multiplicador == '/':
+        print(f'O resuldado da divisão é {calculo_divisao}')
+        
+    else:
+        print('Você precisa digitar algum multiplicador "+", "-", "x" ou "/" para funcionar')
+    
+    continuar = input('Para continuar calculando digite "S" para sim ou "N" para não:')
+    
+    if continuar == 'S':
+        continuartrue = True
+    else:
+        break
+    
+               
